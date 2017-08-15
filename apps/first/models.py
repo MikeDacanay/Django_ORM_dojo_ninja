d7abb8cd64f4d7b1fefca659c02ce9f75682d61d
Mike Dacanay <migd64@gmail.com>
2017-08-15 16:29:47 -0700

    classes from models.py migrated successfully, running queries now

--- apps/first/models.py
+++ apps/first/models.py
@@ -3,10 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Users(models.Model):
+class Dojos(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+class Ninjas(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email_address = models.CharField(max_length=255)
-    age = models.IntegerField()
+    dojos = models.ForeignKey(Dojos, related_name ="ninjas")
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
