38cf377c4e6cb479f90d00d39ce96aa561eb3e66
Mike Dacanay <migd64@gmail.com>
2017-08-15 17:07:43 -0700

    final commit, Ninjas class and Dojos class are properly related. Implemented desc field/att to class as intended

--- apps/first/models.py
+++ apps/first/models.py
@@ -7,6 +7,7 @@ class Dojos(models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
+    desc = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
